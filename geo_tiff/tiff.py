36bd4ec246a626222bf73620e82242b1e5c9dd10
Moleque <molecada@yandex.ru>
2021-05-02 19:19:46 +0300

    composite

--- geo_tiff/tiff.py
+++ geo_tiff/tiff.py
@@ -5,7 +5,7 @@ from pyproj.transformer import Transformer
 from pyproj.crs import CRS
 from geopy.point import Point
 from PIL import Image
-
+import numpy as np
 
 class GeoTiff:
     def __init__(self, path):
@@ -19,6 +19,19 @@ class GeoTiff:
     # получить размеры изображения
     def get_size(self):
         return self.file.width, self.file.height
+
+
+    def get_crs(self):
+        return self.file.crs
+
+
+    def get_transform(self):
+        return self.file.transform
+
+
+    # получить тип данных пикселя
+    def get_dtype(self):
+        return self.file.read(1).dtype
 
 
     # получить координаты крайних точек изображения
@@ -45,35 +58,87 @@ class GeoTiff:
 
 
     # получить numpy отрезок снимка по индексам
-    def get_map_by_indexes(self, width1, height1, width2, height2):
-        return self.file.read(1)[width1:width2, height1:height2]
+    def get_map_by_indexes(self, width1, height1, width2, height2, channel=1):
+        return self.file.read(channel)[height1:height2, width1:width2]
+
+
+    # установить numpy отрезок снимка по индексам TODO check
+    def set_map_by_indexes(self, width, height, slice_map, channel=1):
+        channel_map = self.file.read(channel)
+        channel_map[height1:height2, width1:width2] = slice_map
+        return self.file.write(channel_map, channel)
 
 
     # получить numpy отрезок снимка по координатам
-    def get_map_by_coordinates(self, coordinate1, coordinate2):
+    def get_map_by_coordinates(self, coordinate1, coordinate2, channel=1):
         width1, height1 = self.get_index_by_coordinate(coordinate1)
         width2, height2 = self.get_index_by_coordinate(coordinate2)
-        return self.get_map_by_indexes(width1, height1, width2, height2)
+        return self.get_map_by_indexes(width1, height1, width2, height2, channel)
+
+
+    # изменить значение прозрачности
+    def change_transparency(self, transparency=255, pixel_value=0):
+        if self.file.count < 4:
+            return
+        width, height = self.get_size()
+        tr = np.zeros((height, width), dtype=self.get_dtype())
+        for band in range(1, self.file.count):
+            image_map = self.get_map_by_indexes(0, 0, width, height, band)
+            for h in range(height):
+                for w in range(width):
+                    if image_map[h][w] == pixel_value:
+                        tr[h][w] = transparency      
+        self.file.write(tr, 4)
 
 
     # сохранить часть изображения по индексам
-    def save_image_by_indexes(self, path, width1, height1, width2, height2):
-        image_map = self.get_map_by_indexes(width1, height1, width2, height2)
-        Image.fromarray(image_map, mode='L').save(path)
+    def save_image_by_indexes(self, path, width1, height1, width2, height2, shape=False):
+        width, height = self.get_size()
+        if width1 > width2 or height1 > height2 or width1 > width or height1 > height:
+            return False
+        if width2 > width:
+            if shape:
+                return False
+            width2 = width
+        if height2 > height:
+            if shape:
+                return False
+            height2 = height
+        if self.file.count == 1:
+            mode = "L"
+            image_map = self.get_map_by_indexes(width1, height1, width2, height2)
+        elif self.file.count == 3:
+            mode = "RGB"
+            image_map = np.stack((
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 1), (height2-height1, width2-width1, 1)), 
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 2), (height2-height1, width2-width1, 1)),
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 3), (height2-height1, width2-width1, 1)),
+            ), axis=2)
+        else:
+            mode = "RGBA"
+            image_map = np.stack((
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 1), (height2-height1, width2-width1, 1)), 
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 2), (height2-height1, width2-width1, 1)),
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 3), (height2-height1, width2-width1, 1)),
+                np.reshape(self.get_map_by_indexes(width1, height1, width2, height2, 4), (height2-height1, width2-width1, 1)),
+            ), axis=2)
+        Image.fromarray(image_map, mode=mode).save(path)
+        return True
 
 
     # сохранить часть изображения по координатам
-    def save_image_by_coordinates(self, path, coordinate1, coordinate2):
-        image_map = self.get_map_by_coordinates(coordinate1, coordinate2)
-        Image.fromarray(image_map, mode='L').save(path)
+    def save_image_by_coordinates(self, path, coordinate1, coordinate2, mode="L"):
+        width1, height1 = self.get_index_by_coordinate(coordinate1)
+        width2, height2 = self.get_index_by_coordinate(coordinate2)
+        return self.save_image_by_indexes(width1, height1, width2, height2, mode)
 
 
     # сохранить снимок в виде Georeferenced (по умолчанию перезаписать)
     def save_file_as_georeferenced(self, path=None):
         if path is None:
             path = self.path
-
         shutil.copy(self.file, path, driver='GTiff')
+        return True
 
 
     # перевести метрическую систему координат в географическую
